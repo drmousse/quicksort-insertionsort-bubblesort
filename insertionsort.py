a67dec4e0a3d96cfc842c3adc06ec6992b6d1a19
Musti <42851940+drmousse@users.noreply.github.com>
2018-09-11 01:06:05 +0200

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -13,13 +13,13 @@ def insertionsort(liste):
     n = len(liste)
 
     for i in range(1, n):
-        valuetosort = liste[i]
+        sortValue = liste[i]
         k = i
 
-        while k > 0 and valuetosort < liste[k - 1]:
+        while k > 0 and sortValue < liste[k - 1]:
             liste[k] = liste[k - 1]
             k -=1
-        liste[k] = valuetosort
+        liste[k] = sortValue
 
     return liste
 
